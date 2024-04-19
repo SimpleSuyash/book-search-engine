c3a4a210a1b30cebddf146f1d8d633b64c83bec4
SimpleSuyash <suyashmaharjan@gmail.com>
2024-04-20 04:39:34 +1000

    queries.js added

---
+++ client/src/utils/queries.js
@@ -0,0 +1,39 @@
+import {gql} from '@apollo/client';
+
+export const QUERY_SINGLE_USER = gql`
+    query user($userId: ID!) {
+        user(userId: $userId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const QUERY_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
