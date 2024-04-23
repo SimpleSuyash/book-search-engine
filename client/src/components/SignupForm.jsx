9f37e82061764c9a824b425e3adeb801900593dc
SimpleSuyash <suyashmaharjan@gmail.com>
2024-04-23 15:14:48 +1000

    delete function updated with useQuery

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -52,7 +52,7 @@ const SignupForm = () => {
       */
      
       // /*
-     console.log("1")
+     console.log({ ...userFormData })
       const { data } = await createUser({
         variables: { ...userFormData },
       });
